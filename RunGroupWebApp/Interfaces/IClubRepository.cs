4a824074aed29d10c34d3fe541988b0eea562e14
Dmytro <sagchik9@gmial.com>
2024-03-05 13:56:20 +0000

    club repository interface finished

--- RunGroupWebApp/Interfaces/IClubRepository.cs
+++ RunGroupWebApp/Interfaces/IClubRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace RunGroupWebApp.Interfaces
+﻿using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Interfaces
 {
     public interface IClubRepository
     {
+        Task<IEnumerable<Club>> GetAll();
+        Task<Club> GetByIdAsynk(int id);
+        Task<IEnumerable<Club>> GetClubByCity(string city);
+        bool Add(Club club);
+        bool Update(Club club);
+        bool Delete(Club club);
+        bool Save();
     }
 }
