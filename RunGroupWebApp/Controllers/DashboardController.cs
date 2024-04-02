1e6897c596fb713d5019c518902e82646d0006eb
Dmytro <sagchik9@gmial.com>
2024-04-02 15:35:21 +0100

    dashboard repository with interface added

--- RunGroupWebApp/Controllers/DashboardController.cs
+++ RunGroupWebApp/Controllers/DashboardController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
 
 namespace RunGroupWebApp.Controllers
 {
     public class DashboardController : Controller
     {
-        public DashboardController()
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
         {
-            
+            _context = context;
         }
         public IActionResult Index()
         {
